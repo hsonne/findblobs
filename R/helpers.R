588fd142bc73cd3f98b729f61f4892d94934be30
hsonne <hauke.sonnenberg@gmx.de>
2019-04-14 12:44:49 +0200

    Extract functions into helpers.R

---
+++ R/helpers.R
@@ -0,0 +1,34 @@
+# compare_group_lists ----------------------------------------------------------
+compare_group_lists <- function(gl1, gl2)
+{
+  stopifnot(length(gl1) == length(gl2))
+
+  equals <- sapply(seq_along(gl1), function(i) {
+    identical(gl1[[i]], gl2[[i]])
+  })
+
+  if (any(! equals)) {
+    kwb.utils::printIf(TRUE, which(! equals))
+    kwb.utils::printIf(TRUE, gl1[! equals])
+    kwb.utils::printIf(TRUE, gl2[! equals])
+  }
+}
+
+# order_by_first ---------------------------------------------------------------
+
+#' Order List of Vectors by First Vector Elements
+#'
+#' @param x list of vectors
+#' @examples
+#' order_by_first(list(
+#'   c(3, 4),
+#'   c(1, 5, 6),
+#'   c(2, 1)
+#' ))
+#'
+order_by_first <- function(x)
+{
+  stopifnot(is.list(x))
+
+  x[order(sapply(x, "[[", 1))]
+}
